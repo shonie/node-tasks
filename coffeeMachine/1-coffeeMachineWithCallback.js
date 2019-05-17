9644df72f41822034126e5f5c05cdae530b8bea2
shonie <astarnikov@thesolid.studio>
2019-05-17 11:56:49 +0300

    Added first task

---
+++ coffeeMachine/1-coffeeMachineWithCallback.js
@@ -0,0 +1,31 @@
+const WATER_HEAT_CAPACITY = 4200;
+
+class Machine {
+  enable() {}
+
+  disable() {}
+}
+
+class CoffeeMachine {
+  constructor(power, capacity) {}
+
+  getTimeToBoil() {
+    return this.waterAmount * WATER_HEAT_CAPACITY * 80 / this.power;
+  }
+
+  // Этот метод должен заливать воду в кофемашинку, в нее нельзя залить воды больше ее capacity
+  setWaterAmount(amount) {}
+
+  // Этот метод должен через время расчитанное методом getTimeToBoil вывести в консоль 'Кофе готов' после чего вызвать ф-цию callback
+  // он не должен ничего делать в случае, если машинка выключена
+  run(callback) {}
+}
+
+const coffeeMachine = new CoffeeMachine(100000, 400);
+
+
+coffeeMachine.run(() => console.log(`Let's take some cookies!`)); // Nothing
+
+coffeeMachine.enable();
+
+coffeeMachine.run(() => console.log(`Let's take some cookies!`)); // Now works
