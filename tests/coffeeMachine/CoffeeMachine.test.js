a2463f332fa8dd55b16b34f911cc25d63c3f694a
shonie <astarnikov@thesolid.studio>
2019-05-20 12:34:02 +0300

    Added mocha test

---
+++ tests/coffeeMachine/CoffeeMachine.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const CoffeeMachineWithCallback = require('../../tasks/coffeeMachine/1-CoffeeMachineWithCallback');
+
+describe('CoffeeMachineWithCallback', () => {
+  it('boils coffee in a correct time', () => {
+    const coffeeMachine = new CoffeeMachineWithCallback(100000, 400);
+
+    coffeeMachine.enable();
+
+    coffeeMachine.setWaterAmount(1000);
+
+    const t0 = Date.now();
+
+    coffeeMachine.run(() => {
+      const t1 = Date.now();
+
+      expect(t1 - t0).to.be.at.least(3360);
+    })
+  })
+})
